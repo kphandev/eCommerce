944e2ac25342cc60922a0f82e0b0e2bae8f71716
unknown <thekphantastic@gmail.com>
2021-01-19 23:26:52 -0600

    backend authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  //checks if password has changed. Don't want to rehash password
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
