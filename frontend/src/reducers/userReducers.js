d2a1fe0f9e424a89a5fb4ef39d214301603df305
unknown <thekphantastic@gmail.com>
2021-01-20 23:42:58 -0600

    front-end actions and reducers for login

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,21 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_SUCCESS,
+} from '../constants/userConstants.js'
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true }
+    //this is how the items on the page load, as products is finally set to the payload.
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_LOGOUT:
+      return {}
+    default:
+      return state
+  }
+}
