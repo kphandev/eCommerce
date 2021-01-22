0022317966e79b87946cba6f118e1d5ec7ef861f
unknown <thekphantastic@gmail.com>
2021-01-21 20:48:09 -0600

    update profile funct done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,7 @@ import {
   userLoginReducer,
   userRegisterReducer,
   userDetailsReducer,
+  userUpdateProfileReducer,
 } from './reducers/userReducers'
 
 //these are the states that will show up
@@ -20,6 +21,7 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
