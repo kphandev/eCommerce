607d1bfe18533eefc09ed199812e9584ffaf2458
unknown <thekphantastic@gmail.com>
2021-02-16 20:22:24 -0600

    order constants, reducers, and actions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_REQUEST,
 } from '../constants/orderConstants'
 import { logout } from '../actions/userActions'
 import axios from 'axios'
@@ -78,6 +81,52 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     }
     dispatch({
       type: ORDER_DETAILS_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const payOrder = (orderId, paymentResult) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: ORDER_PAY_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(
+      `/api/orders/${orderId}/pay`,
+      paymentResult,
+      config
+    )
+    console.log('orderAction data: ')
+
+    dispatch({
+      type: ORDER_PAY_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_PAY_FAIL,
       payload: message,
     })
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,6 +15,7 @@ import {
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderPayReducer,
 } from './reducers/orderReducers'
 
 //these are the states that will show up
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
