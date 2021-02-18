b35601ed4e6a1e1845f8d4c8a0188e8fb2a0aeca
unknown <thekphantastic@gmail.com>
2021-02-17 21:19:56 -0600

    added admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 //can only return a single element
 function App() {
@@ -36,6 +37,7 @@ function App() {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/user/:id' component={UserEditScreen} />
           <Route path='/admin/userlist' component={UserListScreen} exact />
+          <Route path='/admin/productlist' component={ProductListScreen} />
         </Container>
       </main>
       <Footer />
