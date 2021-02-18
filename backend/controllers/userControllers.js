7c49df13dbb0ab0653333e8be13f601fbb50668d
unknown <thekphantastic@gmail.com>
2021-02-17 20:12:23 -0600

    edit user Backend done

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -120,6 +120,43 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+// Get user by id
+// get api/users/:id
+// Private / admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// Update User
+// PUT /api/users/:id
+// PRIVATE/admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 export {
   authUser,
   getUserProfile,
@@ -127,4 +164,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
